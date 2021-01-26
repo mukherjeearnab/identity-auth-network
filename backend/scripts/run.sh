c1da22b67d0d129d961c9721912393f5a4b3a3a2
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-01-26 11:19:52 +0530

    Resolved Network issue in peer-base.yaml

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -8,7 +8,7 @@ docker exec -it cli bash ./scripts/channel/create-channel.sh
 docker exec -it cli bash ./scripts/channel/join-peer.sh peer0 identityauthority IdentityAuthorityMSP 8051 1.0
 
 CC="identity_cc"
-
+echo "Installing "$CC
 docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 citizen CitizenMSP 7051 1.0
 docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 identityauthority IdentityAuthorityMSP 8051 1.0
 echo "Instantiating "$CC
