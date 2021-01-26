eb364aca880131f82873b83797a96b9798e684ff
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2021-01-26 10:16:05 +0530

    Resolved MSP naming in run.sh

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -5,12 +5,12 @@ docker-compose -f docker-compose-cli.yaml up -d
 
 docker exec -it cli bash ./scripts/channel/create-channel.sh
 
-docker exec -it cli bash ./scripts/channel/join-peer.sh peer0 identityauthority RTOMSP 8051 1.0
+docker exec -it cli bash ./scripts/channel/join-peer.sh peer0 identityauthority IdentityAuthorityMSP 8051 1.0
 
 CC="identity_cc"
 
 docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 citizen CitizenMSP 7051 1.0
-docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 identityauthority RTOMSP 8051 1.0
+docker exec -it cli bash ./scripts/install-cc/install-onpeer-cc.sh $CC peer0 identityauthority IdentityAuthorityMSP 8051 1.0
 echo "Instantiating "$CC
 docker exec -it cli bash ./scripts/install-cc/instantiate.sh $CC
 
